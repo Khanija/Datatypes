11a5d30a75fa1f4a0cdfb7f318baf48a6fe72f49
Khanija-Git <medakakhanija28@gmail.com>
2022-09-02 17:02:46 +0530

    This is my first commit

---
+++ Datatypes/src/Second/Primitive.java
@@ -0,0 +1,50 @@
+package Second;
+
+public class Primitive {
+
+	public static void main(String[] args)
+	{
+
+		//boolean 
+		boolean IOT = true;
+		System.out.println(IOT);
+		
+		//byte 
+		byte range;
+		range = 127;
+		System.out.println(range);
+		
+		//short
+		short range1;
+		range1 = -127;
+		System.out.println(range1);
+		
+		//int
+		int range2 = 2020020;
+		System.out.println(range2);
+		 
+		//long
+		long range3 = 20202020202020L;
+		System.out.println(range3);
+		
+		//double
+		double number = 99.9;
+		System.out.println(number);
+		 
+		//float
+		float number1 = 98.6f;
+		System.out.println(number1);
+		
+		//char
+		char letter ='y';
+		System.out.println(letter);
+		
+		char letter2 = 89;
+		System.out.println(letter2);
+		
+		char letter3 = 'M';
+		System.out.println(letter3);
+
+	}
+
+}
